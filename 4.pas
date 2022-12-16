aca26d8003b5855a84027e430e32b7696b534462
Konstantin Borisov <68741206+Anexxxus@users.noreply.github.com>
2022-12-16 23:23:33 +0300

    Add files via upload

---
+++ 4.pas
@@ -0,0 +1,22 @@
+﻿var intext, outtext: text;
+  s: string;
+  K,i: integer;
+begin
+  Writeln('Введите число');
+  readln(K);
+  assign(intext,'4.1.txt');
+  assign(outtext,'4.2.txt');
+  reset(intext);
+  rewrite(outtext);
+  i:=0;
+  while not eof(intext) do
+  begin
+    Readln(intext,s);
+   inc(i);
+   if i=K then 
+     Writeln(outtext);
+   Writeln(outtext,s);
+  end;
+  Close(intext);
+  Close(outtext);
+end.
---
+++ 6.pas
@@ -0,0 +1,16 @@
+﻿var intext, outtext: text;
+  s:string;
+begin
+  assign(intext,'6.1.txt');
+  assign(outtext,'6.2.txt');
+  reset(intext);
+  rewrite(outtext);
+  while not eof(intext) do
+  begin
+    Readln(intext,s);
+    if s <> '' then
+      writeln(outtext,s);
+  end;
+  close(intext);
+  close(outtext);
+end.
